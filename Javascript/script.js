588015093be178ed5b3139128ecf1c9c2e75b3e9
Aizabell <109957376+Aizabell@users.noreply.github.com>
2024-03-22 18:20:53 +0630

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,24 @@
+//JavaScript Document
+$(document).ready(function(){
+	$('#autoWidth').lightSlider({
+		autoWidth:true,
+		loop:true,
+		onSliderLoad: function() {
+			$('#autoWidth').removeClass('cs-hidden');
+		}
+	});
+});
+
+//JavaScript Document
+$(document).ready(function(){
+	$('#autoWidth1').lightSlider({
+		autoWidth:true,
+		loop:true,
+		onSliderLoad: function() {
+			$('#autoWidth1').removeClass('cs-hidden');
+		}
+	});
+});
+
+
+
